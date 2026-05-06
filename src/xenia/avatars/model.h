2ff381723fdd982e2b674c7b7aa700f694a4e4d2
Rick Gibbed <rick@gibbed.us>
2026-05-05 19:32:19 -0500

    [Avatars] Split texture out.

--- src/xenia/avatars/model.h
+++ src/xenia/avatars/model.h
@@ -2,7 +2,7 @@
  ******************************************************************************
  * Xenia : Xbox 360 Emulator Research Project                                 *
  ******************************************************************************
- * Copyright 2022 Ben Vanik. All rights reserved.                             *
+ * Copyright 2026 Ben Vanik. All rights reserved.                             *
  * Released under the BSD license - see LICENSE in the root for more details. *
  ******************************************************************************
  */
@@ -15,6 +15,7 @@
 
 #include "xenia/avatars/asset_pack.h"
 #include "xenia/avatars/common.h"
+#include "xenia/avatars/texture.h"
 #include "xenia/base/memory.h"
 
 namespace xe {
@@ -94,32 +95,6 @@ struct TriangleBatch {
                                           ModelLoadOptions load_options);
   static std::vector<uint16_t> ReadIndices(BitStream& stream,
                                            size_t triangle_count);
-};
-
-struct Texture {
- public:
-  uint32_t format;
-  uint32_t width;
-  uint32_t height;
-  uint32_t total_data_size;
-  uint32_t data_size;
-  uint32_t layer_count;
-  bool is_empty;
-  bool is_tiled;
-  uint32_t data_stride;
-  uint32_t data_rows;
-  std::vector<uint8_t> data_bytes;
-
- public:
-  static Texture Read(BitStream& stream);
-
- public:
-  static std::shared_ptr<Texture> Load(const uint8_t* strb_buffer,
-                                       size_t strb_size);
-
- private:
-  static std::shared_ptr<Texture> Read(const uint8_t* data_buffer,
-                                       size_t data_size);
 };
 
 struct ModelTexture {
